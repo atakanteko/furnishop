8eaa7d2b680235f7bbae90d1787c27ce14e15db8
atakantekoglu <atakantekoglu@gmail.com>
2022-06-18 02:37:18 +0300

    feat: home page is completed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Landing from "../components/home-page/Landing";
+import Information from "../components/information/Information";
 
 const Home = () => {
     return (
         <div>
             <Landing />
+            <Information />
         </div>
     );
 };
